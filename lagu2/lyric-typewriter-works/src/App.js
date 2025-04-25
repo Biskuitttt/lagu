bfdc48dac1e4a65848db6ad9fb03d0e19a81d909
peter <biskuithiyahiya@gmail.com>
2025-04-25 21:57:48 +0700

    Resolve merge conflicts

--- lagu2/lyric-typewriter-works/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import TypewriterLyrics from './components/TypewriterLyrics';
-import './style.css';
-
-function App() {
-  return <TypewriterLyrics />;
-}
-
-export default App;
